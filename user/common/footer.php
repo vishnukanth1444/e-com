f93ef27aea16cee0eeb9419fcfdb7e6774d97027
vishnukanth1444 <116946739+vishnukanth1444@users.noreply.github.com>
2024-06-18 15:35:53 +0530

    sidebar and footer

---
+++ user/common/footer.php
@@ -0,0 +1,172 @@
+</main>
+<!-- !start #main-site -->
+
+ <!-- Page footer -->
+ <footer class="footer bg-dark pb-4" data-bs-theme="dark">
+
+<div class="container py-4 py-md-5">
+  <div class="row pt-3 pb-4 py-md-1 py-lg-3">
+
+    <!-- Promo text + Social account links -->
+    <div class="col-lg-3 text-center text-lg-start pb-sm-2 pb-md-0 mb-4 mb-md-5 mb-lg-0">
+      <h4 class="pb-2 mb-1">
+        <a class="text-dark-emphasis text-decoration-none" href="index-2.html">Cartzilla</a>
+      </h4>
+      <p class="fs-sm text-body mx-auto" style="max-width: 480px">With a wide selection of fresh produce, pantry staples, and household essentials, we've got everything you need just a click away.</p>
+      <div class="d-flex justify-content-center justify-content-lg-start gap-2 pt-2 pt-md-3">
+        <a class="btn btn-icon fs-base btn-outline-secondary border-0" href="#!" data-bs-toggle="tooltip" data-bs-template="<div class=&quot;tooltip fs-xs mb-n2&quot; role=&quot;tooltip&quot;><div class=&quot;tooltip-inner bg-transparent text-white p-0&quot;></div></div>" title="Instagram" aria-label="Follow us on Instagram">
+          <i class="ci-instagram"></i>
+        </a>
+        <a class="btn btn-icon fs-base btn-outline-secondary border-0" href="#!" data-bs-toggle="tooltip" data-bs-template="<div class=&quot;tooltip fs-xs mb-n2&quot; role=&quot;tooltip&quot;><div class=&quot;tooltip-inner bg-transparent text-white p-0&quot;></div></div>" title="Facebook" aria-label="Follow us on Facebook">
+          <i class="ci-facebook"></i>
+        </a>
+        <a class="btn btn-icon fs-base btn-outline-secondary border-0" href="#!" data-bs-toggle="tooltip" data-bs-template="<div class=&quot;tooltip fs-xs mb-n2&quot; role=&quot;tooltip&quot;><div class=&quot;tooltip-inner bg-transparent text-white p-0&quot;></div></div>" title="Telegram" aria-label="Follow us on Telegram">
+          <i class="ci-telegram"></i>
+        </a>
+        <a class="btn btn-icon fs-base btn-outline-secondary border-0" href="#!" data-bs-toggle="tooltip" data-bs-template="<div class=&quot;tooltip fs-xs mb-n2&quot; role=&quot;tooltip&quot;><div class=&quot;tooltip-inner bg-transparent text-white p-0&quot;></div></div>" title="WhatsApp" aria-label="Follow us on WhatsApp">
+          <i class="ci-whatsapp"></i>
+        </a>
+      </div>
+    </div>
+
+    <!-- Columns with links that are turned into accordion on screens < 500px wide (sm breakpoint) -->
+    <div class="col-lg-8 offset-lg-1">
+      <div class="accordion" id="footerLinks">
+        <div class="row">
+          <div class="accordion-item col-sm-3 border-0">
+            <h6 class="accordion-header" id="categoriesHeading">
+              <span class="text-dark-emphasis d-none d-sm-block">Categories</span>
+              <button type="button" class="accordion-button collapsed py-3 d-sm-none" data-bs-toggle="collapse" data-bs-target="#categoriesLinks" aria-expanded="false" aria-controls="categoriesLinks">Categories</button>
+            </h6>
+            <div class="accordion-collapse collapse d-sm-block" id="categoriesLinks" aria-labelledby="categoriesHeading" data-bs-parent="#footerLinks">
+              <ul class="nav flex-column gap-2 pt-sm-3 pb-3 pb-sm-0 mt-n1 mb-1 mb-sm-0">
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Weekly sale</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Special price</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Easter is coming</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Italian dinner</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Fresh fruits</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Exotic fruits</a>
+                </li>
+              </ul>
+            </div>
+            <hr class="d-sm-none my-0">
+          </div>
+          <div class="accordion-item col-sm-3 border-0">
+            <h6 class="accordion-header" id="companyHeading">
+              <span class="text-dark-emphasis d-none d-sm-block">Company</span>
+              <button type="button" class="accordion-button collapsed py-3 d-sm-none" data-bs-toggle="collapse" data-bs-target="#companyLinks" aria-expanded="false" aria-controls="companyLinks">Company</button>
+            </h6>
+            <div class="accordion-collapse collapse d-sm-block" id="companyLinks" aria-labelledby="companyHeading" data-bs-parent="#footerLinks">
+              <ul class="nav flex-column gap-2 pt-sm-3 pb-3 pb-sm-0 mt-n1 mb-1 mb-sm-0">
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Blog and news</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">About us</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">FAQ page</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Contact us</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Careers</a>
+                </li>
+              </ul>
+            </div>
+            <hr class="d-sm-none my-0">
+          </div>
+          <div class="accordion-item col-sm-3 border-0">
+            <h6 class="accordion-header" id="accountHeading">
+              <span class="text-dark-emphasis d-none d-sm-block">Account</span>
+              <button type="button" class="accordion-button collapsed py-3 d-sm-none" data-bs-toggle="collapse" data-bs-target="#accountLinks" aria-expanded="false" aria-controls="accountLinks">Account</button>
+            </h6>
+            <div class="accordion-collapse collapse d-sm-block" id="accountLinks" aria-labelledby="accountHeading" data-bs-parent="#footerLinks">
+              <ul class="nav flex-column gap-2 pt-sm-3 pb-3 pb-sm-0 mt-n1 mb-1 mb-sm-0">
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Your account</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Shipping &amp; policies</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Refunds &amp; replacements</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Order tracking</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Delivery info</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Taxes &amp; fees</a>
+                </li>
+              </ul>
+            </div>
+            <hr class="d-sm-none my-0">
+          </div>
+          <div class="accordion-item col-sm-3 border-0">
+            <h6 class="accordion-header" id="customerHeading">
+              <span class="text-dark-emphasis d-none d-sm-block">Customer service</span>
+              <button type="button" class="accordion-button collapsed py-3 d-sm-none" data-bs-toggle="collapse" data-bs-target="#customerLinks" aria-expanded="false" aria-controls="customerLinks">Customer service</button>
+            </h6>
+            <div class="accordion-collapse collapse d-sm-block" id="customerLinks" aria-labelledby="customerHeading" data-bs-parent="#footerLinks">
+              <ul class="nav flex-column gap-2 pt-sm-3 pb-3 pb-sm-0 mt-n1 mb-1 mb-sm-0">
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Payment methods</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Money back guarantee</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Refunds &amp; replacements</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Order tracking</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Delivery info</a>
+                </li>
+                <li class="d-flex w-100 pt-1">
+                  <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="#!">Shipping</a>
+                </li>
+              </ul>
+            </div>
+            <hr class="d-sm-none my-0">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Copyright -->
+<p class="container fs-xs text-body text-center text-lg-start pb-md-3 mb-0">
+  © All rights reserved. Made with <i class="ci-heart-filled align-middle"></i> by <span class="animate-underline"><a class="animate-target text-white text-decoration-none" href="https://chillbot.in" rel="noreferrer">ChillBot</a></span>
+</p>
+</footer>
+
+<!-- Vendor scripts -->
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/simplebar/simplebar.min.js"></script>
+<script src="assets/vendor/choices.js/choices.min.js"></script>
+
+<!-- Bootstrap + Theme scripts -->
+<script src="assets/js/theme.min.js"></script>
+
+
+</body>
+<!-- Mirrored from cartzilla.createx.studio/home-grocery.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 30 May 2024 18:42:10 GMT -->
+</html>
